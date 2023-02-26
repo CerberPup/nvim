90134fb2709f3d5279ee583a53be69af9d94c49d
Wojciech Wrona <wowr130013@gmail.com>
2023-02-26 11:40:00 +0100

    remove ! and man from command tab completion

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -49,12 +49,19 @@ return { -- Autocompletion
 
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
         cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = 'path' },
-                { name = 'cmdline' }
-            })
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = 'path' }
+          }, {
+            {
+              name = 'cmdline',
+              option = {
+                ignore_cmds = { 'Man', '!' }
+              }
+            }
+          })
         })
+
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
         cmp.setup.cmdline({ '/', '?' }, {
             mapping = cmp.mapping.preset.cmdline(),
