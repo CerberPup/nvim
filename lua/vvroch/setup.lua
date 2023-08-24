d344127aaa26b17166bc4cefdc4ff106eda6b05a
Wojciech Wrona <wowr130013@gmail.com>
2023-08-24 14:43:52 +0200

    Remove 'help' from TS installed languages

--- lua/vvroch/setup.lua
+++ lua/vvroch/setup.lua
@@ -78,7 +78,7 @@ end, { desc = '[/] Fuzzily search in current buffer]' })
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript'},
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
