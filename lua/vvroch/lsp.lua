c427538a95e60040e5a0304a5db4d3579ce75c5a
Wojciech Wrona <wowr130013@gmail.com>
2023-09-01 09:14:14 +0200

    Mason codelldb path for rust-tools

--- lua/vvroch/lsp.lua
+++ lua/vvroch/lsp.lua
@@ -94,8 +94,18 @@ require("mason-lspconfig").setup_handlers {
   end,
 }
 
+local mason_registry = require("mason-registry")
+
+local codelldb = mason_registry.get_package("codelldb")
+local extension_path = codelldb:get_install_path() .. "/extension/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+
 local rt = require("rust-tools")
 rt.setup({
+  dap = {
+    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+  },
   server = {
     on_attach = function(_, bufnr)
       -- Hover actions
