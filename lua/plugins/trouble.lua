6c4111b47c5ee7614aa0c378d87398c8d0a7680e
Wojciech Wrona <wowr130013@gmail.com>
2024-01-24 21:21:58 +0100

    Fix descriptions ins trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,42 +1,54 @@
 return {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
-    lazy=true,
-    keys = {
-        {
-            "<leader>et",
-            function() require("trouble").toggle() end,
-            desc = "Toggle trouble",
-        },
-        {
-            "<leader>ew",
-            function() require("trouble").toggle("workspace_diagnostics") end,
-            desc = "Toggle trouble",
-        },
-        {
-            "<leader>ed",
-            function() require("trouble").toggle("document_diagnostics") end,
-            desc = "Toggle trouble",
-        },
-        {
-            "<leader>eq",
-            function() require("trouble").toggle("quickfix") end,
-            desc = "Toggle trouble",
-        },
-        {
-            "<leader>el",
-            function() require("trouble").toggle("loclist") end,
-            desc = "Toggle trouble",
-        },
-        {
-            "gR",
-            function() require("trouble").toggle("lsp_references") end,
-            desc = "Toggle trouble",
-        },
-    }
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	lazy = true,
+	keys = {
+		{
+			"<leader>et",
+			function()
+				require("trouble").toggle()
+			end,
+			desc = "Toggle trouble",
+		},
+		{
+			"<leader>ew",
+			function()
+				require("trouble").toggle("workspace_diagnostics")
+			end,
+			desc = "Workspace",
+		},
+		{
+			"<leader>ed",
+			function()
+				require("trouble").toggle("document_diagnostics")
+			end,
+			desc = "Document",
+		},
+		{
+			"<leader>eq",
+			function()
+				require("trouble").toggle("quickfix")
+			end,
+			desc = "Quickfix",
+		},
+		{
+			"<leader>el",
+			function()
+				require("trouble").toggle("loclist")
+			end,
+			desc = "Loclist",
+		},
+		{
+			"gR",
+			function()
+				require("trouble").toggle("lsp_references")
+			end,
+			desc = "Trouble Reference",
+		},
+	},
 }
