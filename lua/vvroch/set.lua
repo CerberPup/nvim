29fb92b3f6dd9cda9a4a19cf11e35d2303abfae5
Wrona <wojciech.wrona@siemens.com>
2024-02-23 10:00:56 +0100

    Expand dir for undodir

--- lua/vvroch/set.lua
+++ lua/vvroch/set.lua
@@ -31,7 +31,7 @@ vim.o.smartcase = true
 -- files + undodir
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
