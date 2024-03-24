a53ce67923a66be6a75bd9d5a51167d5e83bf006
Wojciech Wrona <wowr130013@gmail.com>
2024-03-24 09:34:13 +0100

    debug

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -79,10 +79,14 @@ return {
   --{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
   -- add telescope-fzf-native
   {
-    "telescope.nvim",
+    "nvim-telescope/telescope.nvim",
+    event = 'VimEnter',
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
       config = function()
         require("telescope").load_extension("fzf")
       end,
